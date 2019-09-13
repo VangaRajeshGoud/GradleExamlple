c9162b73d191089754dc05955d3f84802033e69a
vangarajesh7 <websynergies7@gmail.com>
2019-09-13 12:20:53 +0530

    test commit12

--- src/main/java/com/jcombat/SpringBootApplication.java
+++ src/main/java/com/jcombat/SpringBootApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 @org.springframework.boot.autoconfigure.SpringBootApplication
 public class SpringBootApplication {
 
+	System.out.println("sdf");
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApplication.class, args);
 	}
